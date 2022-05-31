005a250e504b8222df5134231854e7f9aa8001b5
yoshihiroshiakiko <akatsuki79@gmail.com>
2022-05-31 13:44:19 +0900

    サイドメニュー修正

--- js/script.js
+++ js/script.js
@@ -2,14 +2,10 @@
 $(function () {
     $('.p-header__open').on('click', function () {
         $('body').toggleClass('is-open');
-        $('.l-nav').removeClass('nav-close');
-        $('.l-nav').toggleClass('nav-open');
         $('.l-main').toggleClass('overlay');
     });
     $('.p-nav__close').on('click', function () {
         $('body').removeClass('is-open');
-        $('.l-nav').removeClass('nav-open');
-        $('.l-nav').toggleClass('nav-close');
         $('.l-main').removeClass('overlay');
     });
 });
