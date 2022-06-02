11b5f641faee51fbaa39e78a149a0b7a8522ff9c
yoshihiroshiakiko <akatsuki79@gmail.com>
2022-06-02 12:23:08 +0900

    card文字はみ出し、サイドメニュー修正

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ $(function () {
         $('.l-main').removeClass('overlay');
     });
 });
+
+$(window).resize(function () {
+    //windowの幅をxに代入
+    var x = $(window).width();
+    //windowの分岐幅をyに代入
+    var y = 1025;
+    if (x >= y) {
+        $('body').removeClass('is-open');
+        $('.l-main').removeClass('overlay');
+    }
+});
